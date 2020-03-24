ddaef43e642005848aefe180ee43fe5667276c8b
Viviaana <kjingham91@gmail.com>
2020-03-24 11:48:38 +0000

    Updates

---
+++ precache-manifest.0f2bf61c576734fbcf1920dee5ab958a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b50a0f3b561a2d847a9a36dcc31f3d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "168a2b905fd95ef2264a",
+    "url": "/portfolio/static/css/main.7d540d27.chunk.css"
+  },
+  {
+    "revision": "7cab8104a99f198294c4",
+    "url": "/portfolio/static/js/2.163ac616.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/portfolio/static/js/2.163ac616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "168a2b905fd95ef2264a",
+    "url": "/portfolio/static/js/main.c4bdecb3.chunk.js"
+  },
+  {
+    "revision": "0c1932be4300aea2eef2",
+    "url": "/portfolio/static/js/runtime-main.daaa44d4.js"
+  },
+  {
+    "revision": "c0192f92e97d99bf4943bd86e1080bea",
+    "url": "/portfolio/static/media/2playerdice.c0192f92.PNG"
+  },
+  {
+    "revision": "b038b6535660a1bd377670adeeabdc28",
+    "url": "/portfolio/static/media/backgroundplain.b038b653.png"
+  },
+  {
+    "revision": "53f85fbc955e62d1c30230782dc68858",
+    "url": "/portfolio/static/media/blossom.53f85fbc.jpg"
+  },
+  {
+    "revision": "42945683f0629a473707d74c17e14315",
+    "url": "/portfolio/static/media/darkerpinkroses.42945683.jpg"
+  },
+  {
+    "revision": "205e0228e045fcf7b4a972c635a305e5",
+    "url": "/portfolio/static/media/python.205e0228.jpeg"
+  },
+  {
+    "revision": "0ec34439fd6ca57578962537265694dc",
+    "url": "/portfolio/static/media/reactclones.0ec34439.jpg"
+  },
+  {
+    "revision": "9d3fee50fd110027702175ea4c4e3bb6",
+    "url": "/portfolio/static/media/reddit.9d3fee50.jpg"
+  },
+  {
+    "revision": "040f84f6b438dc0bf9d84d5825d7b9d9",
+    "url": "/portfolio/static/media/zen.040f84f6.PNG"
+  }
+]);
