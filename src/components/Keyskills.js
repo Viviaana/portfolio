f2a549d968f3d991cec0e6af721432ddc845b72e
Viviaana <kjingham91@gmail.com>
2020-03-27 12:17:09 +0000

    added skills boxes

--- src/components/Keyskills.js
+++ src/components/Keyskills.js
@@ -5,10 +5,43 @@ export class Keyskills extends Component {
     render() {
         return (
             <div className= "skillscontainer">
-                <div className="skill1">Programs </div>
-                <div className="skill2">Languages</div>
-                <div className="skill3">Office Based</div>
-                <div className="skill4">Personal</div>
+                <div className="first">
+                    <div className="skill1">Software and tools</div>
+                    <div className = "programs">
+                        <ul>
+                            <li>VS Code</li>
+                            <li>Trello</li>
+                            <li>Slack</li>
+                            <li>Github</li>
+                            <li>Microsoft Office</li>
+                            <li>Google Analytics</li>
+                            <li>SAP</li>
+                            <li>Sage</li>
+                            <li>Salesforce</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="second">
+                    <div className="skill2">Languages</div>
+                    <div className = "languages">
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>Javascript</li>
+                            <li>React</li>
+                            <li>Node.js</li>
+                            <li>Python</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="third">
+                    <div className="skill3">Personal</div>
+                    <div className = "personal">
+                        <ul>
+                            <li>Absolutely Nothing</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         )
     }
